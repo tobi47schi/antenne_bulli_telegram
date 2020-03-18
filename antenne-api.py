e8383b304a63f0321aae8e23141d8020a7d04aaa
tobi47schi <tobii12345@gmail.com>
2020-03-18 14:29:40 +0100

    change chatid to string

--- antenne-api.py
+++ antenne-api.py
@@ -88,14 +88,14 @@ def telegram_send_to_channel (text, chat_id) :
     telegram_bot_sendtext(bot_token,chat_id,text)
         
 
-telegram_send_to_channel(die_letzten_zwei_songs() + "\n 01375 / 100 100" ,  -1001363989885)
+telegram_send_to_channel(die_letzten_zwei_songs() + "\n 01375 / 100 100" ,  "-1001363989885")
 
 while(True):
     localtime = time.localtime()
     now = time.strftime("%I:%M:%S %p", localtime)
     print(now)
     if(trefferSongs()):
-        telegram_send_to_channel(die_letzten_zwei_songs() + "\n 01375 / 100 100" ,  -1001363989885)
+        telegram_send_to_channel(die_letzten_zwei_songs() + "\n 01375 / 100 100" ,  "-1001363989885")
         sys.exit()
     time.sleep(5)
 
